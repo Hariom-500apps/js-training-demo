fe211c4bff3d1876b9c58cd6919b1389dfd3b0e1
Hariom-500apps <120471678+Hariom-500apps@users.noreply.github.com>
2022-12-17 18:49:29 +0530

    add code and video

---
+++ Task4/script.js
@@ -0,0 +1,12 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+   return fixedValue+1
+
+  // Only change code above this line
+}
+function getOutput() {
+  document.getElementById("output").innerText= incrementer();
+}
